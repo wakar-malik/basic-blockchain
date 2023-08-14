c3ab79f774af1dbe893804f43ba4068cc0fce2d5
wakar-malik <wakarmalik9@gmail.com>
2023-08-14 23:19:04 +0530

    created global css

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const menuIcon = document.querySelector(".menu-icon");
-const navContainer = document.querySelector(".nav-items__container");
+            const navContainer = document.querySelector(".nav-items__container");
 
-menuIcon.addEventListener("click", function (e) {
-  if (!e.currentTarget.classList.contains("menu-icon")) {
-    return;
-  }
+            menuIcon.addEventListener("click", function (e) {
+              if (!e.currentTarget.classList.contains("menu-icon")) {
+                return;
+              }
 
-  navContainer.classList.toggle("nav-items__container__active");
-});
+              navContainer.classList.toggle("nav-items__container__active");
+            });
